56bf4564fd1ea2273869a9673b8644ec3114585d
abil0 <hoenabil6@gmail.com>
2025-05-27 15:20:12 +0700

    menambahkan ibu & anak

--- pemdas_8/agregasi/agregasi.cpp
+++ pemdas_8/agregasi/agregasi.cpp
@@ -5,5 +5,17 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main()
+int main() {
+    ibu* varIbu = new ibu ("dini");
+    ibu* varIbu2 = new ibu ("novi");
+    anak* varAnak1 = new anak ("tono");
+    anak* varAnak2 = new anak ("rini");
+    anak* varAnak3 = new anak ("dewi");
+
+    varIbu->tambahanak(varAnak1);
+    varIbu->tambahanak(varAnak2);
+    varIbu2->tambahanak(varAnak3);
+    varIbu2->tambahanak(varAnak1);
+    
+}
 
