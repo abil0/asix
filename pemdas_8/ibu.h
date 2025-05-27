860d7fca88dadd4aaa4a1b2825c1d8c47f5da17c
abil0 <hoenabil6@gmail.com>
2025-05-27 14:58:50 +0700

    void ibu

--- pemdas_8/ibu.h
+++ pemdas_8/ibu.h
@@ -10,5 +10,19 @@ class ibu{
         ibu(string pNama): nama(pNama){
             cout << "ibu \"" << nama << "\" ada\n";
         }
-        
+
+        ~ibu(){
+            cout << "ibu \"" << nama << "\" tidak ada\n";
+        }
+        void tambahanak(anak*);
+        void cetakanak();
+      
+};
+void ibu::tambahanak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakanak(){
+    cout << "Daftar Anak dari ibu \"" <<
+    this->nama
 }
