16747939e0091b3a977407bbbd5437833a4dea43
abil0 <hoenabil6@gmail.com>
2025-05-27 15:30:32 +0700

    memnggil 2

--- pemdas_8/asosiasi/asosiasi.cpp
+++ pemdas_8/asosiasi/asosiasi.cpp
@@ -9,6 +9,9 @@ class pasien{
         string nama ;
         vector<dokter*> daftar_dokter;
         pasien(string pNama): nama(pNama) {
-            cout << "pasien \"" << nama << "\" ada\n"
+            cout << "pasien \"" << nama << "\" ada\n";
+        }
+        ~pasien(){
+            cout << "pasien \"" << nama << "\" tidakada\n";
         }
 }
