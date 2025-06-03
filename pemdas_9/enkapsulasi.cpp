36f73d53eb8a7a3f77b03ca7cfefc3244930debc
abil0 <hoenabil6@gmail.com>
2025-06-03 15:07:03 +0700

    tahap 9

--- pemdas_9/enkapsulasi.cpp
+++ pemdas_9/enkapsulasi.cpp
@@ -14,8 +14,11 @@ public:
 };
 
 int main() {
+   remoteLampu lampuRumah:
+   
     remoteLampu.setsaklarNo(0, "lampu teras rumah");
     remoteLampu.setsaklarNo(1, "lampu ruang tamu");
+    remoteLampu.setsaklarNo(2, "lampu kamar tidur");
     remoteLampu.
 
 }
